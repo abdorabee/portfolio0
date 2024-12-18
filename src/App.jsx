257a9381a16467b5944e04f9b966418fc1f22abf
abdorabee <abdorabee1134@gmail.com>
2024-12-18 19:30:15 +0200

    add Loader component and integrate react-three/fiber for 3D rendering in Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import { Home, About, Projects, Contact } from "./pages";
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        <Na vbar />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+/*<div className="absolute top-28 left-0 right-0 z-10">HI</div>*/
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-screen relative ">
+      <Canvas
+        className="w-full h-full relative bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}></Suspense>
+      </Canvas>
+    </section>
+  );
 };
 
 export default Home;
