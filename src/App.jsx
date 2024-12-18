7ca52e1d1e557662ce90417ce7b9c00c4d90fddf
abdorabee <abdorabee1134@gmail.com>
2024-12-18 18:36:52 +0200

    refactor routing and component imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        <Navbar />
+        <Na vbar />
         <Routes>
-          <Route path="/" element={"Home"} />
-          <Route path="/about" element={"About"} />
-          <Route path="/projects" element={"Projects"} />
-          <Route path="/contact" element={"Contact"} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
